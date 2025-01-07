3dd3ef6d6c99aef7bc2e534aee204fa633762518
SamRamirez785 <Samuel.Ramirez@epwu.org>
2025-01-07 10:23:32 -0700

    Add: Water Icon

--- backend/settings.py
+++ backend/settings.py
@@ -42,11 +42,11 @@ class _UiSettings(BaseSettings):
     )
 
     title: str = "H2Know"
-    logo: Optional[str] = None
-    chat_logo: Optional[str] = None
+    logo: Optional[str] = "/water.ico"
+    chat_logo: Optional[str] = "/water.ico"
     chat_title: str = "Start Chatting with H2Know"
     chat_description: str = "This HR assistant is configured to answer your questions relating to any policy"
-    favicon: str = "/favicon.ico"
+    favicon: str = "/water.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
 
