fa8143611331bd5130829688217b78b7faf8c454
SamRamirez785 <Samuel.Ramirez@epwu.org>
2025-01-07 08:32:31 -0700

    Small text change

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,7 @@ class _UiSettings(BaseSettings):
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start Chatting with H2know"
-    chat_description: str = "This HR chatbot is configured to answer your questions relating to any policy"
+    chat_description: str = "This HR assistant is configured to answer your questions relating to any policy"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
